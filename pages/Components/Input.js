0a7a970e41616b458628ea90744d855a1acee8c7
Biniyam <biniyamdaniel.47@gmail.com>
2023-11-03 07:08:29 +0300

    fix: file naming

--- pages/Components/Input.js
+++ pages/Components/Input.js
@@ -39,6 +39,7 @@ const Input = ({
       <input
         id={id}
         disabled={disabled}
+        // {...register(id, { required })}
         placeholder=" "
         type={type}
         className={`
@@ -55,8 +56,12 @@ const Input = ({
           disabled:opacity-70
           disabled:cursor-not-allowed
           ${formatPrice ? "pl-9" : "pl-4"}
-          ${errors[id] ? "border-rose-500" : "border-neutral-300"}
-          ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
+          ${errors && errors[id] ? "border-rose-500" : "border-neutral-300"}
+          ${
+            errors && errors[id]
+              ? "focus:border-rose-500"
+              : "focus:border-black"
+          }
         `}
       />
       <label
