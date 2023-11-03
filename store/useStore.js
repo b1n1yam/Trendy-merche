85e758f0c9f85a01908a2e1ea529b3c106d2d5c9
Biniyam <biniyamdaniel.47@gmail.com>
2023-11-03 06:58:03 +0300

    fix: file path names

--- store/useStore.js
+++ store/useStore.js
@@ -1,28 +1,27 @@
 import { create } from "zustand";
 const useCartStore = create((set) => ({
-    cart: 0,
-    add: () => set((state) => ({ cart: state.cart + 1 })),
-    remove: () => set((state) => ({ cart: state.cart - 1 })),
-    removeAll: () => set({ cart: 0 }),
+  cart: 0,
+  add: () => set((state) => ({ cart: state.cart + 1 })),
+  remove: () => set((state) => ({ cart: state.cart - 1 })),
+  removeAll: () => set({ cart: 0 }),
 }));
 
 export const useLoginModal = create((set) => ({
-    isOpen: false,
-    onOpen: () => set({ isOpen: true }),
-    onClose: () => set({ isOpen: false })
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
 }));
 
 export const useRegisterModal = create((set) => ({
-    isRegisterOpen: false,
-    onRegisterOpen: () => set({ isRegisterOpen: true }),
-    onRegisterClose: () => set({ isRegisterOpen: false })
+  isRegisterOpen: false,
+  onRegisterOpen: () => set({ isRegisterOpen: true }),
+  onRegisterClose: () => set({ isRegisterOpen: false }),
 }));
 export const useIsLogged = create((set) => ({
-    // const Cookiesdata = getCookie('userInfo');
-    isLogged: false,
-    onLogin: () => set({ isLogged: true }),
-    onLogout: () => set({ isLogged: false })
+  // const Cookiesdata = getCookie('userInfo');
+  isLogged: false,
+  onLogin: () => set({ isLogged: true }),
+  onLogout: () => set({ isLogged: false }),
 }));
 
 export default useCartStore;
-
